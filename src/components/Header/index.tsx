eec9d0518dfa414eea2699abd8afbcddcc34553a
Everton Fernandes <evertonfernandes.dev@gmail.com>
2023-01-26 15:40:38 -0300

    feat: :sparkles: application header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,22 @@
-import * as Dialog from '@radix-ui/react-dialog';
+import * as Dialog from '@radix-ui/react-dialog'
 
-import logoImg from '../../assets/logo.svg';
-import { NewTransactionModal } from '../NewTransactionModal';
-import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
+import logoImg from '../../assets/logo.svg'
+import { NewTransactionModal } from '../NewTransactionModal'
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 
 export function Header() {
-    return (
-        <HeaderContainer>
-            <HeaderContent>
-                <img src={logoImg} alt="" />
-                <Dialog.Root>
-                    <Dialog.Trigger asChild >
-                        <NewTransactionButton>Nova Transação</NewTransactionButton>
-                    </Dialog.Trigger >
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
 
-                    <NewTransactionModal />
-                </Dialog.Root>
-            </HeaderContent>
-        </HeaderContainer>
-
-    )
+          <NewTransactionModal />
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
+  )
 }
